02bde7112347015aaf14c018c0dc04c1d0155c81
badigerpraveen <61050804+badigerpraveen@users.noreply.github.com>
2022-09-23 17:25:21 +0530

    code pop model with delete btn
    code pop model with delete btn

--- src/component/Comments.js
+++ src/component/Comments.js
@@ -8,10 +8,50 @@ import userImg from "../images/avatars/image-juliusomo.png";
 import Otherscomments from "./Otherscomments";
 import { Grid } from "@mui/material";
 import { Style } from "@material-ui/icons";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { margin } from "@mui/system";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 280,
+  bgcolor: "background.paper",
+  border: "2px solid gray",
+  borderRadius: "4px",
+  boxShadow: 24,
+  p: 2,
+  color: "gray",
+};  
+
+const styleDeletComent = {
+    color: "#324152" ,
+}
+ 
+const styleBtnDelet = {
+    bgcolor: "#ed6468",
+    '&:hover':{
+     backgroundColor: '#ed6468',
+    }
+} 
+
+const styleBtnCancel = {
+    bgcolor: "#6c757d",
+    '&:hover':{
+     backgroundColor: '#6c757d',
+    }
+}
+
 function Comment() {
   const [message, setMessage] = useState();
   const [isMessage, isSetMessage] = useState([]);
   const [isEditMessage, isSetEdtMessage] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const userData = {
     id: isMessage.length + 1,
@@ -26,6 +66,7 @@ function Comment() {
       return msgId.id !== id;
     });
     isSetMessage(deletMessage);
+    setOpen(false);
   };
 
   const editMessag = (id) => {
@@ -45,10 +86,12 @@ function Comment() {
   console.log(isEditMessage, "isEditMessage");
 
   return (
-    <> 
-     
-      <div className="over"> 
-      <Otherscomments  isEditMessage={isEditMessage} isSetEdtMessage={isSetEdtMessage} />
+    <>
+      <div className="over">
+        <Otherscomments
+          isEditMessage={isEditMessage}
+          isSetEdtMessage={isSetEdtMessage}
+        />
         {isMessage.map((i) => {
           return (
             <Grid
@@ -73,11 +116,13 @@ function Comment() {
                 <Button
                   variant="text"
                   color="error"
-                  onClick={() => deletMessag(i.id)}
+                  //   onClick={() => deletMessag(i.id)}
+                  onClick={handleOpen}
                   startIcon={isEditMessage ? " " : <DeleteIcon />}
                 >
                   {isEditMessage ? " " : "Delet"}
                 </Button>
+
                 <Button
                   variant="text"
                   onClick={() => editMessag(i.id)}
@@ -89,6 +134,53 @@ function Comment() {
               </Grid>
               <Grid item sx={6}>
                 {i.userMessage}
+
+                <div>
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                  >
+                    <Box sx={style}>
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        <strong sx={styleDeletComent}>{"Delete comment"} </strong>
+                      </Typography>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {
+                          "Are you sure you want to delete this comment? This will remove the comment and can’t be undone."
+                        }
+                      </Typography>
+                      <Typography padding={0}>
+                        <item>
+                          <Button sx={styleBtnCancel}
+                            variant="contained"
+                            color="success" 
+                            size="medium"
+                            onClick={handleClose} 
+                            style={{ margin: "10px 0 0 0" }}
+                          >
+                            {"No,Cancel"}
+                          </Button>
+
+                          <Button sx={styleBtnDelet}
+                            variant="contained"
+                            color="error"
+                            size="medium"
+                            onClick={() => deletMessag(i.id)} 
+                            style={{ margin: "10px 0 0 15px" }}
+                          >
+                            {"Yes, Delete"}
+                          </Button>
+                        </item>
+                      </Typography>
+                    </Box>
+                  </Modal>
+                </div>
               </Grid>
               <Grid item xs={11} p={1}>
                 {isEditMessage && i.id ? (
